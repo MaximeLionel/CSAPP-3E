b18c2064ffd2911549eae2ba308ca8619ee6556e
MaximeLionel <ydzhang89@163.com>
2022-03-21 09:54:31 +0800

    202203210954

--- PracticalProblem11_2/hex2dd/csapp.h
+++ PracticalProblem11_2/hex2dd/csapp.h
@@ -4,14 +4,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include "unistd.h"
 #include <string.h>
 #include <ctype.h>
 #include <setjmp.h>
 #include <signal.h>
-#include <sys/time.h>
+#include <time.h>
 #include <sys/types.h>
-#include <sys/wait.h>
+//#include <sys/wait.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/mman.h>
